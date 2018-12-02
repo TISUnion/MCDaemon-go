5877ec6b3f7da925c9ab56232a37d66801bca4dd
lightbrotherV <892217655@qq.com>
2018-12-02 17:09:51 +0800

    test

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/exec"
+)
 
+func main() {
+	cmd := exec.Command("java", "-Xmx1024M", "-Xms1024M", "-jar", "minecraft/server.jar", "nogui")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	defer stdout.Close()
+	res, _ := ioutil.ReadAll(stdout)
+	fmt.Println(string(res))
 }
