989566cee44ce851e4cd9410049b380826f8405e
lightbrother <892217655@qq.com>
2018-12-07 14:43:32 +0800

    例子插件

---
+++ plugins/ExampleYinyinPlugin.go
@@ -0,0 +1,20 @@
+/*
+ * 插件编写例子
+ * 当玩家输入命令：!!yinyinmaster
+ * 他会对所有人说：嘤嘤嘤
+ */
+
+package plugin
+
+import (
+	"MCDaemon-go/command"
+	"MCDaemon-go/lib"
+	"fmt"
+)
+
+type Yinyin struct {
+}
+
+func (hp Yinyin) Handle(c *command.Command, s lib.Server) {
+	s.Say(fmt.Sprintf("%s对所有人说：嘤嘤嘤！", c.Player))
+}
