e2aeaa576be3ab38a6281e74d9e8d420792ea2cf
lightbrother <892217655@qq.com>
2018-12-07 16:05:19 +0800

    添加堵塞所有插件函数

--- lib/server.go
+++ lib/server.go
@@ -1,7 +1,13 @@
 package lib
 
+import "MCDaemon-go/command"
+
 type Server interface {
 	Say(string)
 	Tell(string, string)
 	Execute(string)
+	Close()
+	RunPlugin(*command.Command)
+	LockPluginRuntime()
+	UnlockPluginRuntime()
 }
