94e5a69ba527d0fec61576a36d1b58d4d23d26b7
lightbrother <892217655@qq.com>
2018-12-07 14:44:19 +0800

    插件hash表

---
+++ plugins/PluginMap.go
@@ -0,0 +1,21 @@
+package plugin
+
+import (
+	"MCDaemon-go/config"
+	"MCDaemon-go/lib"
+)
+
+type PluginMap map[string]lib.Plugin
+
+//加载热插件
+func (pm PluginMap) GetHotPlugins(is_reload bool) {
+	plugins := config.GetPlugins(is_reload)
+	for k, v := range plugins {
+		pm[k] = HotPlugin(v)
+	}
+}
+
+//注册热插件
+func (pm PluginMap) RegisterPlugin(name string, lp lib.Plugin) {
+	pm[name] = lp
+}
