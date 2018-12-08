c517a86e5658991bbd5de005969678cbee450406
lightbrotherV <892217655@qq.com>
2018-12-08 23:35:30 +0800

    兼容linux和windows进程通信编码不相同

--- server/stdout.go
+++ server/stdout.go
@@ -1,10 +1,17 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"runtime"
 	"strings"
+	"unicode/utf8"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 //等待服务器加载完地图
@@ -35,7 +42,6 @@ func (svr *Server) WaitEndLoading() {
 //正式运行MCD
 func (svr *Server) Run() {
 	var buffer []byte = make([]byte, 4096)
-	var retStr string
 	for {
 		n, err := svr.Stdout.Read(buffer)
 		if err != nil {
@@ -46,9 +52,26 @@ func (svr *Server) Run() {
 			}
 			break
 		}
-		retStr = string(buffer[:n])
+		var retStr string
+		//注意在window下minecraft进程间通信使用gbk2312;
+		//而在linux下则是utf-8
+		switch runtime.GOOS {
+		case "darwin", "linux":
+			var tempN int
+			var tempStr rune
+			for i := 0; i < n; {
+				tempStr, tempN = utf8.DecodeRune(buffer[i:])
+				retStr += fmt.Sprintf("%c", tempStr)
+				i += tempN
+			}
+		case "windows":
+			reader := transform.NewReader(bytes.NewReader(buffer[:n]), simplifiedchinese.GBK.NewDecoder())
+			retBt, _ := ioutil.ReadAll(reader)
+			retStr = string(retBt)
+		}
+
 		fmt.Println(retStr)
-		//异步处理语法解析器和运行插件
+		// 异步处理语法解析器和运行插件
 		go svr.RunParsers(retStr)
 	}
 }
