4def47e85fdf3891b9e04e260c26fafcee5ad1b6
lightbrother <892217655@qq.com>
2018-12-14 13:53:20 +0800

    接入日志系统

--- server/server.go
+++ server/server.go
@@ -6,11 +6,16 @@ import (
 	parser "MCDaemon-go/parsers"
 	plugin "MCDaemon-go/plugins"
 	"bufio"
+	"fmt"
 	"io"
 	"log"
+	"os"
 	"os/exec"
 	"strconv"
 	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 //单例模式
@@ -28,6 +33,7 @@ type Server struct {
 	lock          sync.Mutex       //输入管道同步锁
 	pulginPool    chan interface{} //插件池
 	maxRunPlugins int              //插件最大并发数
+	log           *logrus.Logger   //日志文件
 	SubServers    []*Server        //保存存档后的镜像（用于之后保存并开启镜像服务器的插件需要）
 }
 
@@ -62,6 +68,10 @@ func (svr *Server) Init(argv []string) {
 	//初始化插件执行池参数
 	svr.maxRunPlugins, _ = strconv.Atoi(config.Cfg.Section("MCDeamon").Key("maxRunPlugins").String())
 	svr.pulginPool = make(chan interface{}, svr.maxRunPlugins)
+
+	//初始化日志类
+	svr.log = logrus.New()
+	svr.log.SetLevel(logrus.DebugLevel)
 }
 
 //运行子进程
@@ -76,6 +86,7 @@ func (svr *Server) RunParsers(word string) {
 		if ok && plugin.PluginsList[cmd.Cmd] != nil {
 			//异步运行插件
 			svr.pulginPool <- 1
+			svr.WriteLog("info", fmt.Sprintf("玩家 %s 运行了 %s 命令", cmd.Player))
 			go svr.RunPlugin(cmd)
 		}
 	}
@@ -100,6 +111,28 @@ func (svr *Server) RunUniquePlugin(handle func()) {
 	svr.pulginPool <- 1
 }
 
+//写入日志
+func (svr *Server) WriteLog(level string, msg string) {
+	logFile, err := os.OpenFile(fmt.Sprintf("logs/%s.log", time.Now().Format("2006-01-02")), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+	defer logFile.Close()
+	if err != nil {
+		fmt.Println("日志写入系统发生错误！ 因为", err)
+	}
+	svr.log.SetOutput(logFile)
+	switch level {
+	case "debug":
+		svr.log.Debug(msg)
+	case "info":
+		svr.log.Info(msg)
+	case "warn":
+		svr.log.Warn(msg)
+	case "error":
+		svr.log.Error(msg)
+	case "fatal":
+		svr.log.Fatal(msg)
+	}
+}
+
 //重启服务器
 func (svr *Server) Restart() {
 	svr.Close()
@@ -112,6 +145,9 @@ func (svr *Server) Restart() {
 	//正式运行MCD
 	svr.Run()
 }
+
+//启动服务器（用于镜像启动）
+func (svr *Server) Start() {}
 
 //关闭服务器
 func (svr *Server) Close() {
