85d1225c69ec9846a0a37fb019d5d6350af8c151
lightbrother <892217655@qq.com>
2018-12-17 15:38:08 +0800

    优化代码

--- command/playerGroup.go
+++ command/playerGroup.go
@@ -27,19 +27,8 @@ func (pg *PlayerGroup) AddPlayer(groupname string, player string) {
 func (pg *PlayerGroup) DelPlayer(groupname string, player string) {
 	pg.lock.Lock()
 	defer pg.lock.Unlock()
-	//是否存在组员
-	HasPlayer := func(groupname string, player string) bool {
-		if len(pg.players) > 0 && pg.players[groupname] != nil {
-			for _, val := range pg.players[groupname] {
-				if val == player {
-					return true
-				}
-			}
-		}
-		return false
-	}
-	//如果组内存在才删除
-	if HasPlayer(groupname, player) {
+	//如果在组内，存在才删除
+	if pg._hasPlayer(groupname, player, false) {
 		var index int
 		for k, val := range pg.players[groupname] {
 			if val == player {
@@ -54,8 +43,20 @@ func (pg *PlayerGroup) DelPlayer(groupname string, player string) {
 
 //查询组内是否有指定玩家
 func (pg *PlayerGroup) HasPlayer(groupname string, player string) bool {
+	return pg._hasPlayer(groupname, player, true)
+}
+
+func (pg *PlayerGroup) GetPlayer() map[string][]string {
 	pg.lock.Lock()
 	defer pg.lock.Unlock()
+	return pg.players
+}
+
+func (pg *PlayerGroup) _hasPlayer(groupname string, player string, isLock bool) bool {
+	if isLock {
+		pg.lock.Lock()
+		defer pg.lock.Unlock()
+	}
 	if len(pg.players) > 0 && pg.players[groupname] != nil {
 		for _, val := range pg.players[groupname] {
 			if val == player {
@@ -64,10 +65,4 @@ func (pg *PlayerGroup) HasPlayer(groupname string, player string) bool {
 		}
 	}
 	return false
-}
-
-func (pg *PlayerGroup) GetPlayer() map[string][]string {
-	pg.lock.Lock()
-	defer pg.lock.Unlock()
-	return pg.players
 }
