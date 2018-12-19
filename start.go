9694e28f44fc3691cea3d8a8e8273186dae5e509
lightbrotherV <892217655@qq.com>
2018-12-19 20:42:39 +0800

    启动镜像放入容器

--- start.go
+++ start.go
@@ -10,30 +10,6 @@ var (
 	MCDeamon []string
 )
 
-// 　　 ┏┓　　  ┏┓
-//　　┏┛┻━━━┛┻┓
-// 　 ┃　　　　　　　 ┃
-// 　 ┃　　　━　　　 ┃
-// 　 ┃　┳┛　┗┳　 ┃
-//　　┃　　　　　　　 ┃
-//　　┃　　　┻　　　 ┃
-//　　┃　　　　　　　 ┃
-//　　┗━┓　　　┏━┛   Codes are far away from bugs with the animal protecting
-//　　　　┃　　　┃       神兽保佑,代码无bug
-//　　　　┃　　　┃
-//　　　　┃　　　┃
-//　　　　┃　　　┃
-//　　　　┃　　　┃
-//　　　　┃　　　┃
-//　　　　┃　　　┃
-//　　　　┃　　　┗━━━┓
-// 　　　  ┃　　　　　 ┣┓
-//　　　　┃　　　　 ┏┛
-//　　　　┗┓┓┏━┳┓┏┛
-//　　　　　┃┫┫　┃┫┫
-//　　　　　┗┻┛　┗┻┛
-//
-
 func init() {
 	//配置eula文件
 	config.SetEula()
