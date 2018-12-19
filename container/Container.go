ebbc58c9ec68c6b20258ef340b667187e3f5c0bc
lightbrother <892217655@qq.com>
2018-12-19 17:47:24 +0800

    添加容器；服务器镜像化

---
+++ container/Container.go
@@ -0,0 +1,58 @@
+package container
+
+import (
+	"MCDaemon-go/lib"
+	"sync"
+)
+
+//单例模式
+var (
+	ContainerInstance *Container
+	once              *sync.Once
+)
+
+func init() {
+	once = &sync.Once{}
+}
+
+//获取单例实例
+func GetInstance() *Container {
+	once.Do(func() {
+		ContainerInstance = &Container{}
+		ContainerInstance.Init()
+	})
+	return ContainerInstance
+}
+
+type Container struct {
+	Servers map[string]lib.Server //所有已启动的服务器
+	lock    sync.Mutex
+}
+
+func (c *Container) Init() {
+	c.Servers = make(map[string]lib.Server)
+}
+
+func (c *Container) Add(name string, ls lib.Server) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.Servers[name] = ls
+}
+
+func (c *Container) Del(name string) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	deleteServer := c.Servers[name]
+	deleteServer.RunUniquePlugin(deleteServer.Close)
+	delete(c.Servers, name)
+}
+
+func (c *Container) GetRuntimeServer() []string {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	var res []string
+	for k, _ := range c.Servers {
+		res = append(res, k)
+	}
+	return res
+}
---
+++ server/plugin.go
@@ -0,0 +1,57 @@
+package server
+
+import (
+	"MCDaemon-go/command"
+	"MCDaemon-go/lib"
+	"fmt"
+	"strings"
+)
+
+//运行所有语法解析器
+func (svr *Server) RunParsers(word string) {
+	for _, val := range svr.parserList {
+		cmd, ok := val.Parsing(word)
+		if ok && svr.pluginList[cmd.Cmd] != nil {
+			//异步运行插件
+			svr.pulginPool <- 1
+			if cmd.Player != "" {
+				svr.WriteLog("info", fmt.Sprintf("玩家 %s 运行了 %s %s命令", cmd.Player, cmd.Cmd, strings.Join(cmd.Argv, " ")))
+			}
+			go svr.RunPlugin(cmd)
+		}
+	}
+}
+
+//运行插件
+func (svr *Server) RunPlugin(cmd *command.Command) {
+	svr.pluginList[cmd.Cmd].Handle(cmd, svr)
+	<-svr.pulginPool
+}
+
+//等待现有插件的完成并停止后面插件的运行，在执行相关操作
+func (svr *Server) RunUniquePlugin(handle func()) {
+	<-svr.pulginPool
+	for i := 0; i < 10; i++ {
+		svr.pulginPool <- 1
+	}
+	handle()
+	for i := 0; i < 10; i++ {
+		<-svr.pulginPool
+	}
+	svr.pulginPool <- 1
+}
+
+//获取当前实例的插件列表
+func (svr *Server) GetPluginList() map[string]lib.Plugin {
+	return svr.pluginList
+}
+
+//获取当前实例的禁用插件列表
+func (svr *Server) GetDisablePluginList() map[string]lib.Plugin {
+	return svr.disablePluginList
+}
+
+//获取语法解析器列表
+func (svr *Server) GetParserList() []lib.Parser {
+	return svr.parserList
+}
