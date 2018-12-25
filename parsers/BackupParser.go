35c91bcee1583106aa3e549407880a49d615c29e
lightbrother <892217655@qq.com>
2018-12-25 17:12:27 +0800

    代码优化

--- parsers/BackupParser.go
+++ parsers/BackupParser.go
@@ -10,7 +10,6 @@ type BackupParser struct{}
 func (p *BackupParser) Parsing(word string) (*command.Command, bool) {
 	re := regexp.MustCompile(`\[\d+:\d+:\d+\]\s+\[Server thread/INFO\]:\s+Saved the game`)
 	match := re.FindStringSubmatch(word)
-	//匹配tps信息时
 	if len(match) != 0 {
 		_commond := &command.Command{
 			Cmd:  "!!backup",
