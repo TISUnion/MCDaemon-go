94c7673e94273dcd9febabd8bc4241d33f771032
scirocco <zyf199966@126.com>
2020-02-27 17:49:03 +0800

    支持了服务器命令行

--- server/stdout.go
+++ server/stdout.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"runtime"
 	"strings"
 	"unicode/utf8"
@@ -37,6 +39,22 @@ func (svr *Server) WaitEndLoading() bool {
 //正式运行MCD
 func (svr *Server) Run() {
 	var buffer []byte = make([]byte, 4096)
+	var buffercmd []byte = make([]byte, 4096)
+	cmdin := bufio.NewReader(os.Stdin)
+	go func() {
+		for {
+			n, err := cmdin.Read(buffercmd)
+			if err != nil {
+				break
+			}
+			cmdStr := Buffer2String(buffercmd, n)
+			if cmdStr[len(cmdStr)-1] == '\n' {
+				cmdStr = cmdStr[0 : len(cmdStr)-1]
+			}
+			svr.Execute(cmdStr)
+		}
+	}()
+
 	for {
 		n, err := svr.Stdout.Read(buffer)
 		if err != nil {
@@ -44,26 +62,33 @@ func (svr *Server) Run() {
 			svr.End()
 			break
 		}
-		var retStr string
-		//注意在window下minecraft进程间通信使用gbk2312;
-		//而在linux下则是utf-8
-		switch runtime.GOOS {
-		case "darwin", "linux":
-			var tempN int
-			var tempStr rune
-			for i := 0; i < n; {
-				tempStr, tempN = utf8.DecodeRune(buffer[i:])
-				retStr += fmt.Sprintf("%c", tempStr)
-				i += tempN
-			}
-		case "windows":
-			reader := transform.NewReader(bytes.NewReader(buffer[:n]), simplifiedchinese.GBK.NewDecoder())
-			retBt, _ := ioutil.ReadAll(reader)
-			retStr = string(retBt)
-		}
+		svrStr := Buffer2String(buffer, n)
 
-		fmt.Println(svr.name, "服务器:", retStr)
+		fmt.Println(svr.name, "服务器:", svrStr)
 		// 异步处理语法解析器和运行插件
-		go svr.RunParsers(retStr)
+		go svr.RunParsers(svrStr)
 	}
+
+}
+
+// Buffer2String :从内存读取字符串
+func Buffer2String(buffer []byte, n int) string {
+	var retStr string
+	//注意在window下minecraft进程间通信使用gbk2312;
+	//而在linux下则是utf-8
+	switch runtime.GOOS {
+	case "darwin", "linux":
+		var tempN int
+		var tempStr rune
+		for i := 0; i < n; {
+			tempStr, tempN = utf8.DecodeRune(buffer[i:])
+			retStr += fmt.Sprintf("%c", tempStr)
+			i += tempN
+		}
+	case "windows":
+		reader := transform.NewReader(bytes.NewReader(buffer[:n]), simplifiedchinese.GBK.NewDecoder())
+		retBt, _ := ioutil.ReadAll(reader)
+		retStr = string(retBt)
+	}
+	return retStr
 }
